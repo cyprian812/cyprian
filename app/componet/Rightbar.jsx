1af9a0bea58e1d592a302adb16baeff3d9d96e05
cyprian812 <cyprianameh@gmail.com>
2025-11-30 18:43:52 +0100

    Rightbar

--- app/componet/Rightbar.jsx
+++ app/componet/Rightbar.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import Image from 'next/image'
+import Online from './Sidebar/Online';
+import { Users } from '@/dummyData';
+
 
 export default function Rightbar() {
   return (
-    <div className='hidden md:flex'>
-      this is rightbar
+    <div className='hidden lg:block w-[30vw] font-semibold overflow-hidden'>
+    <div className="pt-[20px] pr-[20px]">
+      <div className="flex items-center">
+  <Image
+            src="/assets/team/hennessyad.jpg"
+            alt="hennessyad"
+            width={40}
+            height={40}
+            className="w-[40px] h-[40px] mr-[10px]"
+          />
+      <span className='text-[15px]'>
+        <b>daniel Paul</b> and <b>3 other friends</b> this
+      </span>
+      </div>
+      <div className="">
+        <Image
+            src="/assets/team/hennessyad.jpg"
+            alt="hennessyad"
+            width={500}
+            height={500}
+            className="w-full rounded-md object-contain my-[30px] hover:scale-110 transition-transform duration-300 overflow-hidden "
+          />
+      </div>
+      <h4 className='mb-[20px]'>Online Friends</h4>
+     <ul>
+       {Users.map((u) => (
+        <Online key={u.id} user ={u} />
+      ))}
+     </ul>
+    </div>
     </div>
   )
 }
